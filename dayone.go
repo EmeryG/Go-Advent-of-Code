37a87e896377593a44c96dbe0a02f39bfddc7c8f
Emery Griffith <emerytgriffith@gmail.com>
2024-01-18 16:23:27 -0600

    Add 2nd part of Day 1 challenge

--- dayone.go
+++ dayone.go
@@ -17,6 +17,47 @@ func getFileContents(filename string) string {
 	return string(data)
 }
 
+// Converts first and last occurrence of each alphabetic digit from 1-9
+func convertLetterDigits(str string) string {
+	writtenToDigits := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	insertNumericDigits := make(map[int]string)
+
+	// Iterate through each written digit and store starting index in addDigits map
+	for writtenDigit, numericDigit := range writtenToDigits {
+		indexWritten := strings.Index(str, writtenDigit)
+
+		if indexWritten > -1 {
+			insertNumericDigits[indexWritten] = numericDigit
+		}
+
+		lastIndexWritten := strings.LastIndex(str, writtenDigit)
+
+		if lastIndexWritten > -1 {
+			insertNumericDigits[lastIndexWritten] = numericDigit
+		}
+	}
+
+	var convertedStr string = str
+
+	// Replace starting letter of written digit with numeric digit
+	for index, numericDigit := range insertNumericDigits {
+		convertedStr = convertedStr[:index] + numericDigit + convertedStr[index+1:]
+	}
+
+	return convertedStr
+}
+
 func getCalibration(calibrationDoc string) int {
 	// List of number characters
 	numChars := "1234567890"
@@ -29,9 +70,12 @@ func getCalibration(calibrationDoc string) int {
 	for _, line := range lines {
 		var numberBuilder strings.Builder
 
+		// Convert alpabetic digits to numbers
+		convertedLine := convertLetterDigits(line)
+
 		// Find char of first and last number in string
-		firstNumChar := line[strings.IndexAny(line, numChars)]
-		lastNumChar := line[strings.LastIndexAny(line, numChars)]
+		firstNumChar := convertedLine[strings.IndexAny(convertedLine, numChars)]
+		lastNumChar := convertedLine[strings.LastIndexAny(convertedLine, numChars)]
 
 		numberBuilder.WriteByte(firstNumChar)
 		numberBuilder.WriteByte(lastNumChar)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Go Advent of Code
+
+Hi, these are my solutions to Advent of Code using Go as a learning exercise.
+
+[Try Advent of Code here](https://adventofcode.com/2023)
